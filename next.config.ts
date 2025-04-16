33eb0b36dd70f681034145782730c4076f79459f
Carlos Chao <chao@yareytech.com>
2025-04-15 21:33:35 -0400

    fix: remove velite config from next config and add npm-run-all to start

--- next.config.ts
+++ next.config.ts
@@ -9,25 +9,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.plugins.push(new VeliteWebpackPlugin());
-    return config;
-  },
 };
 
-class VeliteWebpackPlugin {
-  static started = false
-  apply(compiler: { hooks: { beforeCompile: { tapPromise: (arg0: string, arg1: () => Promise<void>) => void; }; }; options: { mode: string; }; }) {
-    // executed three times in nextjs
-    // twice for the server (nodejs / edge runtime) and once for the client
-    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-      if (VeliteWebpackPlugin.started) return
-      VeliteWebpackPlugin.started = true
-      const dev = compiler.options.mode === 'development'
-      const { build } = await import('velite')
-      await build({ watch: dev, clean: !dev })
-    })
-  }
-}
 
 export default nextConfig;
