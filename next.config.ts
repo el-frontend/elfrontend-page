68ef0d9401c0febccba3ab4ab355199e0c94d48d
Carlos Chao <chao@yareytech.com>
2025-04-15 21:28:36 -0400

    fix: update velite config in nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { build } from "velite";
 
 const nextConfig: NextConfig = {
   images: {
@@ -17,14 +16,17 @@ const nextConfig: NextConfig = {
 };
 
 class VeliteWebpackPlugin {
-  static started = false;
+  static started = false
   apply(compiler: { hooks: { beforeCompile: { tapPromise: (arg0: string, arg1: () => Promise<void>) => void; }; }; options: { mode: string; }; }) {
-    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
-      if (VeliteWebpackPlugin.started) return;
-      VeliteWebpackPlugin.started = true;
-      const dev = compiler.options.mode === "development";
-      await build({ watch: dev, clean: !dev });
-    });
+    // executed three times in nextjs
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      const { build } = await import('velite')
+      await build({ watch: dev, clean: !dev })
+    })
   }
 }
 
