9fcac2541664b1605c8f3c6b0f6729469bca784f
Carlos Chao <chao@yareytech.com>
2025-05-05 14:44:03 -0400

    feat: update google analytics env var

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,12 @@ export default function RootLayout({
         <Toaster />
         <Analytics />
         <SpeedInsights />
-        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA_API_KEY ?? ""} />
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID &&
+          process.env.NODE_ENV === "production" && (
+            <GoogleTagManager
+              gtmId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}
+            />
+          )}
       </body>
     </html>
   );
